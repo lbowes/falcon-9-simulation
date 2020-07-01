a234463e99218bff0c655147fc5b949b586bcc0f
Lewis Bowes <lewisbowes0@gmail.com>
2020-07-01 20:50:51 +0100

    Complete new basic structure for Scene class

--- visualisation/include/Scene.h
+++ visualisation/include/Scene.h
@@ -1,27 +1,31 @@
-#ifndef SCENE_H_
-#define SCENE_H_
+#ifndef F9SIM_GRAPHICS_SCENE_H_
+#define F9SIM_GRAPHICS_SCENE_H_
+#pragma once
 
-#include "SimulationModel.h"
-#include "cameras/FPVCamera.h"
 
-#include <ISceneManager.h>
-#include <IrrlichtDevice.h>
+namespace F9Sim {
+namespace Graphics {
 
 
 class Scene {
-private:
-    bool mCamerasHaveFocus;
-    SimulationModel mSimModel;
-    FPVCamera testCam;
-
 public:
-    Scene(irr::scene::ISceneManager& sceneMgr, irr::video::IVideoDriver& vidDriver);
-    ~Scene() = default;
+    static Scene& getInstance();
+    void draw() const;
 
-    void handleInput(double dt);
-    void update(float aspectRatio, double dt);
+    Scene(const Scene&) = delete;
+    Scene(Scene&&) = delete;
+    void operator=(const Scene&) = delete;
+    void operator=(Scene&&) = delete;
+
+private:
+    Scene() = default;
+    ~Scene() = default;
 };
 
 
-#endif // SCENE_H_
+} // namespace Graphics
+} // namespace F9Sim
+
+
+#endif // F9SIM_GRAPHICS_SCENE_H_
 
