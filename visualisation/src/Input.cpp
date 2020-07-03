84c97db37c829f04bf66c15645ac133538444975
Lewis Bowes <lewisbowes0@gmail.com>
2020-07-03 15:51:55 +0100

    Make `Input` members all static

--- visualisation/src/Input.cpp
+++ visualisation/src/Input.cpp
@@ -1,174 +1,171 @@
 #include "Input.h"
 
 #include <GLFW/glfw3.h>
-// temp
-#include <iostream>
-//
 
 
 namespace F9Sim {
 
 
-static const uint16_t MAX_KEYS = 512;
-static const uint8_t MAX_BUTTONS = 16;
-static bool cursorHidden = false;
-static bool keysDown[MAX_KEYS];
-static bool keysDown_last[MAX_KEYS];
-static bool keysPressed[MAX_KEYS];
-static bool keysReleased[MAX_KEYS];
-static bool buttonsDown[MAX_BUTTONS];
-static bool buttonsPressed[MAX_BUTTONS];
-static bool buttonsReleased[MAX_BUTTONS];
-static bool buttonsDown_last[MAX_BUTTONS];
-static glm::ivec2 mousePos_last = {0, 0};
-static glm::ivec2 mousePos = {0, 0};
-static glm::ivec2 mouseDelta = {0, 0};
-static GLFWwindow* window = nullptr;
+const uint16_t Input::s_maxKeys;
+const uint8_t Input::s_maxButtons;
+bool Input::m_cursorHidden = false;
+glm::ivec2 Input::m_mousePos_last = {0, 0};
+glm::ivec2 Input::m_mousePos = {0, 0};
+glm::ivec2 Input::m_mouseDelta = {0, 0};
+GLFWwindow* Input::m_window = nullptr;
+bool Input::m_keysDown[s_maxKeys];
+bool Input::m_keysDown_last[s_maxKeys];
+bool Input::m_keysPressed[s_maxKeys];
+bool Input::m_keysReleased[s_maxKeys];
+bool Input::m_buttonsDown[s_maxButtons];
+bool Input::m_buttonsPressed[s_maxButtons];
+bool Input::m_buttonsReleased[s_maxButtons];
+bool Input::m_buttonsDown_last[s_maxButtons];
 
 
-static void setCallbacks();
+static void setCallbacks(GLFWwindow* win);
 static void keyCallback(GLFWwindow* win, int key, int scanCode, int action, int mods);
 static void buttonCallback(GLFWwindow* win, int button, int scanCode, int action);
 static void mousePosCallback(GLFWwindow* win, double x, double y);
 
 
-void Input_init(GLFWwindow& win) {
-    window = &win;
+void Input::init(GLFWwindow& win) {
+    m_window = &win;
 
-    for(uint16_t i = 0; i < MAX_KEYS; i++) {
-        keysDown[i] = false;
-        keysPressed[i] = false;
-        keysReleased[i] = false;
-        keysDown_last[i] = false;
+    for(uint16_t i = 0; i < s_maxKeys; i++) {
+        m_keysDown[i] = false;
+        m_keysPressed[i] = false;
+        m_keysReleased[i] = false;
+        m_keysDown_last[i] = false;
     }
 
-    for(uint8_t i = 0; i < MAX_BUTTONS; i++) {
-        buttonsDown[i] = false;
-        buttonsPressed[i] = false;
-        buttonsReleased[i] = false;
-        buttonsDown_last[i] = false;
+    for(uint8_t i = 0; i < s_maxButtons; i++) {
+        m_buttonsDown[i] = false;
+        m_buttonsPressed[i] = false;
+        m_buttonsReleased[i] = false;
+        m_buttonsDown_last[i] = false;
     }
 
-    setCallbacks();
+    setCallbacks(m_window);
 }
 
 
-static void setCallbacks() {
-    glfwSetKeyCallback(window, keyCallback);
-    glfwSetMouseButtonCallback(window, buttonCallback);
-    glfwSetCursorPosCallback(window, mousePosCallback);
+void Input::setCallbacks(GLFWwindow* win) {
+    glfwSetKeyCallback(win, keyCallback);
+    glfwSetMouseButtonCallback(win, buttonCallback);
+    glfwSetCursorPosCallback(win, mousePosCallback);
 }
 
 
-static void keyCallback(GLFWwindow* win, int key, int scanCode, int action, int mods) {
-    keysDown[key] = (key >= 0) && (key < MAX_KEYS) && (action == GLFW_PRESS);
+void Input::keyCallback(GLFWwindow* win, int key, int scanCode, int action, int mods) {
+    m_keysDown[key] = (key >= 0) && (key < s_maxKeys) && (action == GLFW_PRESS);
 }
 
 
-static void buttonCallback(GLFWwindow* win, int button, int action, int mods) {
-    buttonsDown[button] = (button >= 0) && (button < MAX_BUTTONS) && (action == GLFW_PRESS);
+void Input::buttonCallback(GLFWwindow* win, int button, int action, int mods) {
+    m_buttonsDown[button] = (button >= 0) && (button < s_maxButtons) && (action == GLFW_PRESS);
 }
 
 
-static void mousePosCallback(GLFWwindow* win, double x, double y) {
-    mousePos = {x, y};
+void Input::mousePosCallback(GLFWwindow* win, double x, double y) {
+    m_mousePos = {x, y};
 
     static bool firstCall = true;
     if(firstCall) {
         firstCall = false;
-        mousePos_last = mousePos;
+        m_mousePos_last = m_mousePos;
     }
 }
 
 
-void Input_update() {
-    mouseDelta = mousePos - mousePos_last;
-    mousePos_last = mousePos;
+void Input::update() {
+    m_mouseDelta = m_mousePos - m_mousePos_last;
+    m_mousePos_last = m_mousePos;
 
-    for(uint16_t i = 0; i < MAX_KEYS; i++) {
-        keysPressed[i] = !keysDown_last[i] && keysDown[i];
-        keysReleased[i] = keysDown_last[i] && !keysDown[i];
-        keysDown_last[i] = keysDown[i];
+    for(uint16_t i = 0; i < s_maxKeys; i++) {
+        m_keysPressed[i] = !m_keysDown_last[i] && m_keysDown[i];
+        m_keysReleased[i] = m_keysDown_last[i] && !m_keysDown[i];
+        m_keysDown_last[i] = m_keysDown[i];
     }
 
-    for(uint8_t i = 0; i < MAX_BUTTONS; i++) {
-        buttonsPressed[i] = !buttonsDown_last[i] && buttonsDown[i];
-        buttonsReleased[i] = buttonsDown_last[i] && !buttonsDown[i];
-        buttonsDown_last[i] = buttonsDown[i];
+    for(uint8_t i = 0; i < s_maxButtons; i++) {
+        m_buttonsPressed[i] = !m_buttonsDown_last[i] && m_buttonsDown[i];
+        m_buttonsReleased[i] = m_buttonsDown_last[i] && !m_buttonsDown[i];
+        m_buttonsDown_last[i] = m_buttonsDown[i];
     }
 }
 
 
-bool Input_isKeyDown(uint16_t key) {
-    return key < MAX_KEYS && keysDown[key];
+bool Input::isKeyDown(uint16_t key) {
+    return key < s_maxKeys && m_keysDown[key];
 }
 
 
-bool Input_isKeyUp(uint16_t key) {
-    return key < MAX_KEYS && !keysDown[key];
+bool Input::isKeyUp(uint16_t key) {
+    return key < s_maxKeys && !m_keysDown[key];
 }
 
 
-bool Input_isKeyPressed(uint16_t key) {
-    return key < MAX_KEYS && keysPressed[key];
+bool Input::isKeyPressed(uint16_t key) {
+    return key < s_maxKeys && m_keysPressed[key];
 }
 
 
-bool Input_isKeyReleased(uint16_t key) {
-    return key < MAX_KEYS && keysReleased[key];
+bool Input::isKeyReleased(uint16_t key) {
+    return key < s_maxKeys && m_keysReleased[key];
 }
 
 
-bool Input_isMouseButtonDown(uint8_t button) {
-    return button < MAX_BUTTONS && buttonsDown[button];
+bool Input::isMouseButtonDown(uint8_t button) {
+    return button < s_maxButtons && m_buttonsDown[button];
 }
 
 
-bool Input_isMouseButtonUp(uint8_t button) {
-    return button < MAX_BUTTONS && !buttonsDown[button];
+bool Input::isMouseButtonUp(uint8_t button) {
+    return button < s_maxButtons && !m_buttonsDown[button];
 }
 
 
-bool Input_isMouseButtonPressed(uint8_t button) {
-    return button < MAX_BUTTONS && buttonsPressed[button];
+bool Input::isMouseButtonPressed(uint8_t button) {
+    return button < s_maxButtons && m_buttonsPressed[button];
 }
 
 
-bool Input_isMouseButtonReleased(uint8_t button) {
-    return button < MAX_BUTTONS && buttonsReleased[button];
+bool Input::isMouseButtonReleased(uint8_t button) {
+    return button < s_maxButtons && m_buttonsReleased[button];
 }
 
 
-bool Input_isCursorHidden() {
-    return cursorHidden;
+bool Input::isCursorHidden() {
+    return m_cursorHidden;
 }
 
 
-glm::ivec2 Input_getMousePos() {
-    return mousePos;
+glm::ivec2 Input::getMousePos() {
+    return m_mousePos;
 }
 
 
-glm::ivec2 Input_getMouseDelta() {
-    return mouseDelta;
+glm::ivec2 Input::getMouseDelta() {
+    return m_mouseDelta;
 }
 
 
-void Input_hideMouseCursor() {
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    cursorHidden = true;
+void Input::hideMouseCursor() {
+    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    m_cursorHidden = true;
 }
 
 
-void Input_showMouseCursor() {
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    cursorHidden = false;
+void Input::showMouseCursor() {
+    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    m_cursorHidden = false;
 }
 
 
-void Input_setCursorPos(glm::ivec2 pos) {
-    glfwSetCursorPos(window, pos.x, pos.y);
-    mousePos = pos;
+void Input::setCursorPos(glm::ivec2 pos) {
+    glfwSetCursorPos(m_window, pos.x, pos.y);
+    m_mousePos = pos;
 }
 
 
